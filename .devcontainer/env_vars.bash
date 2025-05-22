90ef9c82d4b3bfaf0964cdcec9c13142b1fcf79d
Kiril Ivanov <ivanovkirilg@gmail.com>
2025-05-22 19:01:17 +0000

    Add devcontainer config

---
+++ .devcontainer/env_vars.bash
@@ -0,0 +1,7 @@
+
+SOURCE_DIR="/workspaces/big-bad-voodoo-daddy/src"
+BUILD_DIR="/workspaces/big-bad-voodoo-daddy/build"
+TEST_DIR="/workspaces/big-bad-voodoo-daddy/tests"
+TEST_OUTPUT_DIR="$BUILD_DIR/test_output"
+
+ROBOT_OPTIONS="--outputdir $TEST_OUTPUT_DIR"
