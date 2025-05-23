7acdf6203bbc9d95754290055791dced25aa7923
GermanGermanov <germanov_german@abv.bg>
2025-05-23 12:46:15 +0000

    vend exercise

--- tests/test_VendingMachine.cpp
+++ tests/test_VendingMachine.cpp
@@ -8,3 +8,49 @@ TEST(TestVendingMachine, Dummy)
     
     ASSERT_EQ(vendingMachine.getBalance(), 0);
 }
+
+TEST(TestVendingMachine, GetStock)
+{
+    VendingMachine vendingMachine;
+    const std::string itemValid    = "Soda";
+
+    vendingMachine.stockItem("Soda", 5, 50);
+
+    ASSERT_EQ(5, vendingMachine.getItemStock(itemValid));
+}
+
+TEST(TestVendingMachine, GetStockNotValid)
+{
+    VendingMachine vendingMachine;
+    const std::string itemNotValid = "Whiskey";
+
+    vendingMachine.stockItem("Soda", 5, 50);
+
+    EXPECT_THROW(vendingMachine.getItemStock(itemNotValid), VendingMachine::VendingMachineError);
+}
+
+TEST(TestVendingMachine, GetItemPrice)
+{
+    VendingMachine vendingMachine;
+    const std::string Item      = "Soda";
+    int               itemPrice = 50;
+
+    vendingMachine.stockItem(Item, 5, itemPrice);
+
+    EXPECT_EQ(vendingMachine.getItemPrice(Item), itemPrice);
+}
+
+TEST(TestVendingMachine, GetItemPriceNotValid)
+{
+    VendingMachine vendingMachine;
+    const std::string Item           = "Soda";
+    int               itemPrice      = 50;
+    int               itemPriceWrong = 30;
+
+    vendingMachine.stockItem(Item, 5, itemPrice);
+
+    EXPECT_THROW(vendingMachine.getItemPrice("Cola"), VendingMachine::VendingMachineError);
+}
+
+
+
--- tests/test_vend.robot
+++ tests/test_vend.robot
@@ -1,9 +1,13 @@
 *** Settings ***
+Library  Process
+
+
+*** Settings ***
 Resource   resources/run_program.resource
 Resource   resources/match_output.resource
 
 *** Test Cases ***
 Vend runs
-    Fail  missing a keyword for input
-    # When Program vend is executed successfully
-    # Then the output shall contain  Welcome to the Vending Machine!
+    When Program vend is executed successfully
+    Then the output shall contain  Goodbye
+
