4266e2a9ef4bbf65f5b68ab018e0af9033371390
VIGeorgiev <viktor88georgiev@gmail.com>
2025-05-23 13:00:47 +0000

    bulshit

--- tests/test_VendingMachine.cpp
+++ tests/test_VendingMachine.cpp
@@ -2,9 +2,27 @@
 
 #include "gtest/gtest.h"
 
-TEST(TestVendingMachine, Dummy)
+TEST(TestVendingMachine, get_balance)
 {
     VendingMachine vendingMachine;
     
     ASSERT_EQ(vendingMachine.getBalance(), 0);
 }
+
+TEST(TestVendingMachine, insert_coin_correct)
+{
+    VendingMachine vendingMachine;
+    auto money = 25;
+    vendingMachine.insertCoin(money);
+
+    ASSERT_EQ(vendingMachine.getBalance(), money);
+}
+
+TEST(TestVendingMachine, insert_coin_negative_number)
+{
+    VendingMachine vendingMachine;
+    auto money = -25;
+
+    EXPECT_THROW(vendingMachine.insertCoin(money), VendingMachine::VendingMachineError );
+}
+
--- tests/test_vend.robot
+++ tests/test_vend.robot
@@ -2,8 +2,18 @@
 Resource   resources/run_program.resource
 Resource   resources/match_output.resource
 
+
+
+*** Variables ***
+
+
 *** Test Cases ***
 Vend runs
-    Fail  missing a keyword for input
-    # When Program vend is executed successfully
-    # Then the output shall contain  Welcome to the Vending Machine!
+    # Fail  missing a keyword for input
+    Given set test input
+    ...  Soda\n
+    ...  200\n
+    ...  exit\n
+    When Program vend is executed successfully
+    Then the output shall contain  Welcome to the Vending Machine!
+
